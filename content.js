6fc5bad32b56bdef8e7b83c5a02eb24cd13bfec3
Download Shell Backdoor <31315581+Wanheda0602@users.noreply.github.com>
2025-04-23 13:41:55 +0700

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,5 @@
-document.write('<div>');
-document.write('<div style="width: 400px; margin: 0 auto;">');
+document.write('<div style="width: 400px;">');
 document.write('<scr' + 'ipt type="text/javascript" async="async" data-cfasync="false" src="//genderorganiccensus.com/ecfa413cb925900c95ef6bf0451b2edb/invoke.js"></scr' + 'ipt>');
 document.write('<div id="container-ecfa413cb925900c95ef6bf0451b2edb">');
-document.write('</div>');
 document.write('</div>');
 document.write('</div>');
